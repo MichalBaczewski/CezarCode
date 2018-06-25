f061499f9601d796cccd7c9c277aa5fd58be786c
MichalBaczewski <michal.baczewski@outlook.com>
2018-06-25 22:16:51 +0200

    Dodano obsługę wyjątków.

--- src/Cezar.java
+++ src/Cezar.java
@@ -1,31 +1,39 @@
+import java.io.IOException;
+
 public class Cezar {
 
-    public String encryptText(String inputText) {
+    public String encryptText(String inputText) throws IOException {
         char[] inputTextTable = inputText.toCharArray();
         char[] codedTextTable = new char[inputText.length()];
-        int tmp = 0;
+        int tmp;
         for (int index = 0; index < inputTextTable.length; index++) {
             tmp = (int) inputTextTable[index];
             if (tmp > 64 && tmp < 88) {
                 tmp += 3;
             } else if (tmp > 87 && tmp < 91) {
                 tmp -= 23;
+            } else if (tmp == 32) {
+            } else {
+                throw new IOException();
             }
             codedTextTable[index] = (char) tmp;
         }
         return new String(codedTextTable);
     }
 
-    public String decryptText(String inputText) {
+    public String decryptText(String inputText) throws IOException {
         char[] inputTextTable = inputText.toCharArray();
         char[] uncodedTextTable = new char[inputText.length()];
-        int tmp = 0;
+        int tmp;
         for (int index = 0; index < inputTextTable.length; index++) {
             tmp = (int) inputTextTable[index];
             if (tmp > 67 && tmp < 91) {
                 tmp -= 3;
             } else if (tmp > 64 && tmp < 68) {
                 tmp += 23;
+            } else if (tmp == 32) {
+            } else {
+                throw new IOException();
             }
             uncodedTextTable[index] = (char) tmp;
         }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         String inputText = "KURS PROGRAMOWANIA W JAVIE";
         System.out.println("Wiadomość: " + inputText);
         Cezar cezarCode = new Cezar();
-        String encryptedText = cezarCode.encryptText(inputText);
-        System.out.println("Zakodowana wiadomość: " + encryptedText);
-        String decryptedText = cezarCode.decryptText(encryptedText);
-        System.out.println("Odkodowana wiadomość: " + decryptedText);
+        try {
+            String encryptedText = cezarCode.encryptText(inputText);
+            System.out.println("Zakodowana wiadomość: " + encryptedText);
+            String decryptedText = cezarCode.decryptText(encryptedText);
+            System.out.println("Odkodowana wiadomość: " + decryptedText);
+        } catch (IOException e) {
+            System.out.println("Program operuje tylko na WIELKICH literach od A do Z");
+        }
     }
 
 }
